67796450df30b35a86a5f6c25deb086fbf40bae2
megan <msmeganmcmanus@gmail.com>
2020-09-05 16:55:31 -0700

    completed project

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from forex_python.converter import CurrencyRates, CurrencyCodes, Decimal, get_rate
+from forex_python.converter import CurrencyRates, CurrencyCodes, Decimal
 import math
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "327-446-427"
@@ -10,6 +9,15 @@ debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 c = CurrencyRates(force_decimal=True)
 cc = CurrencyCodes()
+
+
+def get_rate(con_from, con_to, amount):
+    '''Handles the rate conversion to 2 decimals and gets currency symbol.'''
+    try:
+        rate = (cc.convert(con_from, con_to, amount))
+        return rate
+    except:
+        return False
 
 
 @app.route("/")
@@ -21,14 +29,16 @@ def home_page():
 @app.route("/convert")
 def convert():
     """gets input, converts via forex converter methods, renders response template or redirect to error"""
+
     try:
         con_from = request.args["convert_from"]
         con_to = request.args["convert_to"]
         amount = Decimal(request.args["amount"])
 
-        converted_amt = get_rate(from_code, to_code, float(amount))
+        converted_amt = c.convert(con_from, con_to, amount)
+        display_amt = round(converted_amt, 2)
 
-        return render_template("rate.html", con_from=con_from, con_to=con_to, converted_amt=converted_amt)
+        return render_template("rate.html", display_amt=display_amt, con_from=con_from, con_to=con_to)
     except:
         return redirect(url_for('error'))
 
